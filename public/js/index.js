85cca157807b0e8db1980f38444b027c4b0920ec
JTTran <tranjt80@gmail.com>
2017-07-11 00:59:50 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -12,16 +12,53 @@ socket.on("newMessage", function (message) {
     console.log("newMessage", message);
     const li = jQuery("<li></li>");
     li.text(`${message.from}: ${message.text}`);
+
     jQuery("#messages").append(li);
 });
+
+socket.on("newLocationMessage", function (message) {
+    const li = jQuery("<li></li>");
+    const a = jQuery("<a target='_blank'>My current location</a>");
+    
+    li.text(`${message.from}:`);
+    a.attr("href", message.url);
+    li.append(a);
+    jQuery("#messages").append(li);
+
+});
+
+const messageTextBox = jQuery("[name=message]");
 
 jQuery("#message-form").on("submit", function (event) {
     event.preventDefault();
 
+    
+
     socket.emit("createMessage", {
         from: "User",
-        text: jQuery("[name=message]").val()
+        text: messageTextBox.val()
     }, function () {
+        messageTextBox.val("");
+    });
+});
 
+const locationButton = jQuery("#send-location");
+
+locationButton.on("click", function (event) {
+    if (!navigator.geolocation) {
+        return alert("Geolocation not supported by your browser.");
+    }
+
+    locationButton.attr("disabled", "disabled").text("Sending location...");
+
+    navigator.geolocation.getCurrentPosition(function (position) {     
+        locationButton.removeAttr("disabled").text("Send location");
+        socket.emit("createLocationMessage", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {    
+        locationButton.removeAttr("disabled").text("Send location");
+        alert("Unable to fetch location");
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,16 +1,27 @@
 const expect = require("expect");
 
-const { generateMessage } = require("./message");
+const { generateMessage, generateLocationMessage } = require("./message");
 
 describe("generateMessage", () => {
     it("should generate the correct message object", () => {
         const from = "test";
-        const text = "test text"
+        const text = "test text";
         const message = generateMessage(from, text);
-
-        expect(message.from).toBe("test");
-        expect(message.text).toBe("test text");
+        
         expect(message.createdAt).toBeA("number");
         expect(message).toInclude({from, text});
+    });
+});
+
+describe("generateLocationMessage", () => {
+    it("should generate correct location object", () => {
+        const from ="test";
+        const latitude = 15;
+        const longitude = 19;
+        const url = "https://www.google.com/maps?q=15,19";
+
+        const message = generateLocationMessage(from, latitude, longitude);
+        expect(message.createdAt).toBeA("number");        
+        expect(message).toInclude({from, url});
     });
 });
