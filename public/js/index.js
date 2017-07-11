706d0a02934e818a830c688c77b4ab666451f957
JTTran <tranjt80@gmail.com>
2017-07-11 15:25:49 +0300

    Add mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,30 +10,32 @@ socket.on("disconnect", function () {
 
 socket.on("newMessage", function (message) {
     const formattedTime = moment(message.createdAt).format("h:mm a");
-    const li = jQuery("<li></li>");
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    const template = jQuery("#message-template").html();
+    const html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery("#messages").append(li);
+    jQuery("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function (message) {
     const formattedTime = moment(message.createdAt).format("h:mm a");
-    const li = jQuery("<li></li>");
-    const a = jQuery("<a target='_blank'>My current location</a>");
-    
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr("href", message.url);
-    li.append(a);
-    jQuery("#messages").append(li);
+    const template = jQuery("#location-message-template").html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
+    jQuery("#messages").append(html);
 });
 
 const messageTextBox = jQuery("[name=message]");
 
 jQuery("#message-form").on("submit", function (event) {
-    event.preventDefault();
-
-    
+    event.preventDefault();    
 
     socket.emit("createMessage", {
         from: "User",
